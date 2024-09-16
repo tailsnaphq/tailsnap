6d0a4ee8251bcc03c1cf0527c8b572b6734f5a51
Tailsnap <info.tailsnap@gmail.com>
2024-09-16 16:51:49 -0400

    Component work

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ module.exports = plugin(function ({ addComponents }) {
         '.btn-basic-outline': {
             '@apply px-2.5 py-1.5 rounded-lg w-fit border-2 border-[#111005]': {},
         },
+
+        // btn-arrow
+        '.btn-arrow': {
+            '@apply p-2 rounded-lg bg-[#111005] text-[#FFFFFF] flex items-center gap-2 w-fit': {}, // Base styles
+            'position': 'relative', // Necessary for ::before positioning
+            '&::before': {
+                'content': '"\\2192"', // Unicode for an arrow (can replace this with a custom SVG or image)
+                'display': 'inline-block',
+                'background-color': '#A0E60D', // Icon background
+                'width': '24px',
+                'height': '24px',
+                'border-radius': '4px',
+                'text-align': 'center',
+                'line-height': '24px',
+                'color': '#000',
+                'font-size': '16px',
+            },
+        },
     };
 
     addComponents(newComponents);
