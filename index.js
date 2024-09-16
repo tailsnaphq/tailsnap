1619e4e982658104b10720fa7738bb92f9c5e377
Tailsnap <info.tailsnap@gmail.com>
2024-09-16 08:45:37 -0400

    Component work

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(function ({ addUtilities }) {
     const newUtilities = {
-        '.text-shadow': {
-            'text-shadow': '2px 2px 2px rgba(0, 0, 0, 0.4)',
-        },
-        '.text-shadow-md': {
-            'text-shadow': '4px 4px 6px rgba(0, 0, 0, 0.6)',
-        },
-        '.text-shadow-lg': {
-            'text-shadow': '6px 6px 8px rgba(0, 0, 0, 0.8)',
-        },
-        '.test': {
-            'color': '#AD452F',
+        // Buttons ----------------------------------
+        '.btn': {
+            'padding': '0.5rem',
+            'background-color': '#000000',
+            'color': '#ffffff',
+            'width': 'fit-content',
+            'border-radius': '0.375rem',
         },
     };
 
