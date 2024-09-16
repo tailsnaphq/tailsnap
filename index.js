89f75e2e65ffc14462a80adc1c1a55d288bccc46
Tailsnap <info.tailsnap@gmail.com>
2024-09-16 15:40:56 -0400

    Layouts test

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(function ({ addComponents }) {
     const newComponents = {
-        // Buttons
-        '.btn': {
-            '@apply p-2 bg-black text-white rounded-md w-fit': {},
+
+        // LAYOUTS ---------------------------------------------------------------------------------------------------------
+
+        // flex-center-all
+        '.flex-center-all': {
+            '@apply flex items-center justify-center': {},
+        },
+
+        // BUTTONS ---------------------------------------------------------------------------------------------------------
+
+        // btn-basic
+        '.btn-basic': {
+            '@apply px-2.5 py-1.5 rounded-lg w-fit border-2 border-[#111005] bg-[#111005] text-[#FFFFFF]': {},
+        },
+
+        // btn-basic-outline
+        '.btn-basic-outline': {
+            '@apply px-2.5 py-1.5 rounded-lg w-fit border-2 border-[#111005]': {},
         },
     };
 
