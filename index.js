98ebc8abacea09c78243ea6c9015bb7c47d36005
Tailsnap <info.tailsnap@gmail.com>
2024-09-20 21:50:56 -0400

    Select profiles config

--- index.js
+++ index.js
@@ -110,13 +110,13 @@ module.exports = plugin(function ({ addComponents }) {
             '@apply flex items-center gap-2': {},
         },
         '.select-profiles-button': {
-            '@apply flex items-center justify-between bg-transparent p-2.5 border rounded-md w-full': {},
+            '@apply flex items-center justify-between bg-transparent p-2.5 border-2 rounded-lg w-full': {},
         },
         '.select-profiles-arrow': {
             '@apply ml-2 text-gray-500': {},
         },
         '.select-profiles-dropdown': {
-            '@apply absolute w-full bg-transparent border mt-1 hidden z-10 rounded-md shadow-md': {},
+            '@apply absolute w-full bg-transparent border-2 mt-1 hidden z-10 rounded-lg': {},
         },
         '.select-profiles.open .select-profiles-dropdown': {
             '@apply block': {},
